31148e3fdda20e257553379b79e17c2bce7d658f
manikishorekamanaboina <158121738+manikishorekamanaboina@users.noreply.github.com>
2024-01-30 09:56:39 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,15 +1,20 @@
 def question6_1():
-    """
     answer = {
-        'equal_width': ['string', integer],
-        'equal_frequency': ['string', integer]
+        'equal_width': ['No change', 9],
+        'equal_frequency': ['No change', 9]
     }
-    """
     return answer
 
 def question6_2():
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['No Change', 9]
+    }
     return answer
 
 def question6_3():
-    return answer 
-
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['No Change', 9]
+    } 
+    return answer
