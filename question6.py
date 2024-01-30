24760e6b23193c8bd8368e4359d02e5ffd6d51ce
manikishorekamanaboina <158121738+manikishorekamanaboina@users.noreply.github.com>
2024-01-30 10:13:45 +0530

    Add files via upload

---
+++ question6.py
@@ -0,0 +1,21 @@
+def question6_1():
+    answer = {
+        'equal_width': ['No change', 9],
+        'equal_frequency': ['No change', 9]
+    }
+    return answer
+
+def question6_2():
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['No Change', 9]
+    }
+    return answer
+
+def question6_3():
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['No Change', 9]
+    } 
+    return answer
+
