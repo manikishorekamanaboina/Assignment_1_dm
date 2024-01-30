d124eb1153b7b3178c787c8e0f7d46d3f68ef2bc
manikishorekamanaboina <158121738+manikishorekamanaboina@users.noreply.github.com>
2024-01-30 09:55:14 +0530

    Add files via upload

---
+++ Question6.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "62e7709f-0c42-476b-a1f5-5d5db990197f",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def question6_1():\n",
+    "    answer = {\n",
+    "        'equal_width': ['No change', 9],\n",
+    "        'equal_frequency': ['No change', 9]\n",
+    "    }\n",
+    "    return answer\n",
+    "\n",
+    "def question6_2():\n",
+    "    answer = {\n",
+    "        'equal_width': ['Change', 2],\n",
+    "        'equal_frequency': ['No Change', 9]\n",
+    "    }\n",
+    "    return answer\n",
+    "\n",
+    "def question6_3():\n",
+    "    answer = {\n",
+    "        'equal_width': ['Change', 2],\n",
+    "        'equal_frequency': ['Change', 4]\n",
+    "    } \n",
+    "    return answer"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
--- question4.py
+++ question4.py
@@ -1,24 +1,65 @@
-def question4_1():
-    # answer = ['string', 'string']
-    return answer
-
-def question4_2():
-    return answer
-
-def question4_3():
-    return answer 
-
-def question4_4():
-    return answer
-
-def question4_5():
-    return answer
-
-def question4_6():
-    return answer
-
-def question4_7():
-    return answer
-
-def question4_8():
-    return answer
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "197b69dd-8ce0-4391-b235-a2ffc4d8104b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def question4_1():\n",
+    "    answer = ['nominal', 'oedinal']\n",
+    "    return answer\n",
+    "\n",
+    "def question4_2():\n",
+    "    answer = ['interval', 'ordinal']\n",
+    "    return answer\n",
+    "\n",
+    "def question4_3():\n",
+    "    answer = ['ratio', 'ordinal'] \n",
+    "    return answer\n",
+    "\n",
+    "def question4_4():\n",
+    "    answer = ['ratio', 'ordinal']\n",
+    "    return answer\n",
+    "\n",
+    "def question4_5():\n",
+    "    answer = ['ratio', 'ratio']\n",
+    "    return answer\n",
+    "\n",
+    "def question4_6():\n",
+    "    answer = ['ratio', 'ordinal']\n",
+    "    return answer\n",
+    "\n",
+    "def question4_7():\n",
+    "    answer = ['ratio', 'ratio']\n",
+    "    return answer\n",
+    "\n",
+    "def question4_8():\n",
+    "    answer = ['ratio', 'interval']\n",
+    "    return answer"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
