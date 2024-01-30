f2e99016f89d4a911a2ca1b1d3e471a3551acff0
manikishorekamanaboina <158121738+manikishorekamanaboina@users.noreply.github.com>
2024-01-30 09:58:26 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,65 +1,31 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "197b69dd-8ce0-4391-b235-a2ffc4d8104b",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def question4_1():\n",
-    "    answer = ['nominal', 'oedinal']\n",
-    "    return answer\n",
-    "\n",
-    "def question4_2():\n",
-    "    answer = ['interval', 'ordinal']\n",
-    "    return answer\n",
-    "\n",
-    "def question4_3():\n",
-    "    answer = ['ratio', 'ordinal'] \n",
-    "    return answer\n",
-    "\n",
-    "def question4_4():\n",
-    "    answer = ['ratio', 'ordinal']\n",
-    "    return answer\n",
-    "\n",
-    "def question4_5():\n",
-    "    answer = ['ratio', 'ratio']\n",
-    "    return answer\n",
-    "\n",
-    "def question4_6():\n",
-    "    answer = ['ratio', 'ordinal']\n",
-    "    return answer\n",
-    "\n",
-    "def question4_7():\n",
-    "    answer = ['ratio', 'ratio']\n",
-    "    return answer\n",
-    "\n",
-    "def question4_8():\n",
-    "    answer = ['ratio', 'interval']\n",
-    "    return answer"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+def question4_1():
+    answer = ['nominal', 'oedinal']
+    return answer
+
+def question4_2():
+    answer = ['interval', 'ordinal']
+    return answer
+
+def question4_3():
+    answer = ['ratio', 'ordinal'] 
+    return answer
+
+def question4_4():
+    answer = ['ratio', 'ordinal']
+    return answer
+
+def question4_5():
+    answer = ['ratio', 'ratio']
+    return answer
+
+def question4_6():
+    answer = ['ratio', 'ordinal']
+    return answer
+
+def question4_7():
+    answer = ['ratio', 'ratio']
+    return answer
+
+def question4_8():
+    answer = ['ratio', 'interval']
+    return answer
