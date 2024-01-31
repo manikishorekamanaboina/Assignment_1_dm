8c48d839375d869cd5e91df3db00b670f7103baa
manikishorekamanaboina <158121738+manikishorekamanaboina@users.noreply.github.com>
2024-01-30 22:54:27 -0500

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,19 @@
+def question2_1():
+    answer = ['quantitative', 'discrete', 'ratio']
+    return answer
+
+def question2_2():
+    answer = ['quantitative', 'continuous', 'ratio']
+    return answer
+
+def question2_3():
+    answer = ['quantitative', 'continuous', 'interval']
+    return answer 
+
+def question2_4():
+    answer = ['qualitative', 'ordinal', 'discrete']
+    return answer
+
+def question2_5():
+    answer = ['qualitative', 'nominal', 'discrete']
+    return answer
